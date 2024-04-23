9961ad91478c9b7dc8c0ce027e696fd0bee47819
Ruby Kendrick <kendrick.ruby@gmail.com>
2024-04-23 10:46:41 -0500

    classes defined, collections initialized

---
+++ Models/DTOS/FollowerDTO.cs
@@ -0,0 +1,8 @@
+namespace RustyAnchorsApi.Models.DTOS;
+
+public class FollowerDTO
+{
+    public int Id { get; set; }
+    public int PirateId {get; set;}
+    public int FollowerId {get; set;}
+}
---
+++ Models/DTOS/StoryDTO.cs
@@ -0,0 +1,10 @@
+namespace RustyAnchorsApi.Models.DTOS;
+
+public class StoryDTO
+{
+    public int Id { get; set; }
+    public int PirateId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+    public DateTime Date { get; set; }
+}
