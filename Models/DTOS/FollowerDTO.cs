055c2c5f04735df67a42acdea7434618e14cfb4f
Ruby Kendrick <kendrick.ruby@gmail.com>
2024-04-24 11:01:56 -0500

    endpoint for getting a pirates favorite pirates

--- Models/DTOS/FollowerDTO.cs
+++ Models/DTOS/FollowerDTO.cs
@@ -3,6 +3,7 @@ namespace RustyAnchorsApi.Models.DTOS;
 public class FollowerDTO
 {
     public int Id { get; set; }
-    public int PirateId {get; set;}
-    public int FollowerId {get; set;}
+    public int PirateId { get; set; }
+    public int FollowerId { get; set; }
+    public PirateDTO? Pirate { get; set; }
 }
